2982a5a4465d1a4d4f40919ca4f4668f5956d5bf
David <dluna.dev@gmail.com>
2025-11-02 20:06:03 -0300

    feat: add password validations and scale size helpers

---
+++ src/helpers/password-validate.ts
@@ -0,0 +1,8 @@
+export const messages = {
+  isLengthValid: 'Entre 8 y 20 caracteres',
+  hasLowerCase: 'Al menos una letra minúscula',
+  hasUpperCase: 'Al menos una letra mayúscula',
+  hasNumber: 'Al menos un número',
+  hasSpecialChar: 'Al menos un carácter especial',
+  passwordEqual: 'Ambas contraseñas deben ser iguales',
+};
---
+++ src/helpers/scale-size.ts
@@ -0,0 +1,5 @@
+import { moderateScale } from 'react-native-size-matters';
+
+export const scaleSize = (size: number) => {
+  return moderateScale(size);
+};
