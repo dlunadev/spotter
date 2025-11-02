de1bd14f78d57d3c2f07642ac790e86672caded4
David <dluna.dev@gmail.com>
2025-11-02 20:04:41 -0300

    feat: add svg icons

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,27 @@
+import Begin from './begin.svg';
+import Calendar from './calendar.svg';
+import Check from './check.svg';
+import ChevronDown from './chevron-down.svg';
+import CircleIcon from './circle-alert.svg';
+import Clock from './clock.svg';
+import EyeOffIcon from './eye-off.svg';
+import EyeIcon from './eye.svg';
+import Machin from './logo_machin.svg';
+import Logout from './logout.svg';
+import Pin from './map-pin.svg';
+import PauseIcon from './pause-icon.svg';
+import Pause from './pause.svg';
+import Play from './play.svg';
+import Resumed from './resumed.svg';
+import Shift from './shift.svg';
+import Square from './square.svg';
+import Cross from './cross.svg';
+import ArrowLeft from './arrow-left.svg';
+
+export {
+  Begin, Calendar, Check, ChevronDown, CircleIcon, Clock, EyeIcon,
+  EyeOffIcon, Logout,
+  Machin,
+  Pause, PauseIcon, Pin, Play, Resumed, Shift, Square, Cross, ArrowLeft
+};
+
