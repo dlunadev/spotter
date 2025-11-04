0d7b6f9d2d3dc533d2a629e5cc557704c53dc7b4
David <dluna.dev@gmail.com>
2025-11-03 22:54:31 -0300

    refactor: update wrapper component

--- src/components/atom/wrapper/wrapper.component.tsx
+++ src/components/atom/wrapper/wrapper.component.tsx
@@ -1,9 +1,10 @@
 import { View } from "react-native";
 import React, { ReactElement } from "react";
 import { useInsets } from "@/hooks";
+import { Colors } from "@/constants/Colors";
 
-export default function Wrapper({children, className }: {children: ReactElement | ReactElement[], className?: string}) {
+export  function Wrapper({children, className }: {children: ReactElement | ReactElement[], className?: string}) {
   const { top, bottom } = useInsets();
 
-  return <View style={{ paddingTop: top, paddingBottom: bottom, flex: 1  }} className={`${className} p-6`}>{children}</View>;
+  return <View style={{ paddingTop: top, paddingBottom: bottom, flex: 1, backgroundColor: Colors.WHITE  }} className={`${className} p-6`}>{children}</View>;
 }
