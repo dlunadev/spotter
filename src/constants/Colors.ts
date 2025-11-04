bd06efef1409ac7ee05378a48588f853a071bb42
David <dluna.dev@gmail.com>
2025-11-03 22:56:27 -0300

    refactor: update colors constant

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,26 +1,19 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  PRIMARY: '#F23D4C',
+  SECONDARY: '#2EC4B6',
+  WHITE: '#FFFFFF',
+  TERTIARY: '#ACB5BB',
+  BLACK: '#000000',
+  TEXT: '#0D0D0D',
+  PINK: '#F7BBBE',
+  LIGHT_PINK: '#F29199',
+  GRAY: '#6C7278',
+  LIGHT_GRAY: '#F2F2F2',
+  GREEN: '#34C759',
+  YELLOW: '#FFCC00',
+  BLUE: '#007AFF',
+  LIGHT_GREEN: '#34C75959',
+  LIGHT_YELLOW: '#FFCC0059',
+  LIGHT_BLUE: '#007AFF59',
+  INPUT: "#EDF1F3"
 };
