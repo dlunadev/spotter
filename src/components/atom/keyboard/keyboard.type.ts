c1af6dc3158bd3ebc1b28a6679c0f84751b75709
David <dluna.dev@gmail.com>
2025-11-03 22:51:13 -0300

    delete: remove keyboard component

--- src/components/atom/keyboard/keyboard.type.ts
+++
@@ -1,9 +0,0 @@
-export type KeyboardContainerProps = {
-  children: React.ReactNode;
-  behavior?: 'padding' | 'height' | 'position';
-  style?: object;
-  keyboardShouldPersistTaps?: 'always' | 'handled' | 'never';
-  extraScrollHeight?: number;
-  enableOnAndroid?: boolean;
-  extraHeight?: number;
-}
