7768a17d459aca541839b4c2e95fa5eb744cc7f5
David <dluna.dev@gmail.com>
2025-11-09 23:26:54 -0300

    feat: add translations to sign in and update sign up translations

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,17 +2,21 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
 
-import en_auth from './locales/en/auth/sign-up/translation.json';
-import es_auth from './locales/es/auth/sign-up/translation.json';
+import en_auth_signup from './locales/en/auth/sign-up/translation.json';
+import es_auth_signup from './locales/es/auth/sign-up/translation.json';
+import en_auth_signin from './locales/en/auth/sign-in/translation.json';
+import es_auth_signin from './locales/es/auth/sign-in/translation.json';
 
 const resources = {
   en: {
-    auth_signup: en_auth,
+    auth_signup: en_auth_signup,
+    auth_signin: en_auth_signin,
   },
   es: {
-    auth_signup: es_auth
+    auth_signup: es_auth_signup,
+    auth_signin: es_auth_signin,
   },
-}
+};
 
 const language_code = Localization.getLocales()[0]?.languageCode || 'en';
 
@@ -23,7 +27,7 @@ i18n
     lng: language_code,
     fallbackLng: 'en',
     resources,
-    ns: ['auth_signup'],
+    ns: ['auth_signup', 'auth_signin'],
     interpolation: {
       escapeValue: false,
     },
