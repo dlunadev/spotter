e060b3ce72b46aae367210a5983638c370468037
David <dluna.dev@gmail.com>
2025-11-11 21:21:29 -0300

    refactor: remove home stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -46,7 +46,6 @@ function RootLayoutNav() {
           <KeyboardProvider>
             <Stack initialRouteName="(auth)">
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="(home)" options={{ headerShown: false }} />      
             </Stack>
           </KeyboardProvider>
         </GestureHandlerRootView>
