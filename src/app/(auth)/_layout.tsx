3ac07975111ea27048db92148f4ec36dce5ae335
David <dluna.dev@gmail.com>
2025-11-11 21:24:18 -0300

    feat: add new routes

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -9,9 +9,9 @@ export default function _layout() {
         <Stack.Screen name={AuthRoutes.SIGN_IN} options={{ headerShown: false }} />
         <Stack.Screen name={AuthRoutes.SIGN_UP} options={{ headerShown: false }} />
         <Stack.Screen name={AuthRoutes.RECOVERY_PASSWORD} options={{ headerShown: false }} />
-        {/* <Stack.Screen name={AuthRoutes.} options={{ headerShown: false }} />
-        <Stack.Screen name={AuthRoutes.CONFIRMATION} options={{ headerShown: false }} />
-        <Stack.Screen name={AuthRoutes.NEW_PASSWORD} options={{ headerShown: false }} /> */}
+        <Stack.Screen name={AuthRoutes.SEND_CODE} options={{ headerShown: false }} />
+        <Stack.Screen name={AuthRoutes.RESET_PASSWORD} options={{ headerShown: false }} />
+        <Stack.Screen name={AuthRoutes.SUCESS} options={{ headerShown: false }} />
       </Stack>
     </>
   );
--- src/utils/enum/routes.ts
+++ src/utils/enum/routes.ts
@@ -1,25 +1,21 @@
 import { Href } from "expo-router";
 
 export enum AuthRoutes {
-  SIGN_IN = "sign-in",
-  SIGN_UP = "sign-up",
-  RECOVERY_PASSWORD = "recovery-password",
-  SEND_CODE = "send-code",
+  SIGN_IN = "sign-in/index",
+  SIGN_UP = "sign-up/index",
+  RECOVERY_PASSWORD = "password/recovery-password/index",
+  SEND_CODE = "password/code/index",
   RESET_PASSWORD = "reset-password",
-  PASSWORD_SUCCESS = "password-success",
-  ADDITIONAL_INFO = "additional-info",
-  MAP = "map"
+  SUCESS = "password-success",
 }
 
 export const AuthRoutesLink = {
   SIGN_IN: "/(auth)/sign-in" as Href,
   SIGN_UP: "/(auth)/sign-up" as Href,
-  RECOVERY_PASSWORD: "/(auth)/recovery-password" as Href,
-  SEND_CODE: "/(auth)/send-code" as Href,
-  RESET_PASSWORD: "/(auth)/reset-password" as Href,
-  PASSWORD_SUCCESS: "/(auth)/password-success" as Href,
-  ADDITIONAL_INFO: "/(auth)/additional-info" as Href,
-  MAP: "/(auth)/map" as Href,
+  RECOVERY_PASSWORD: "/(auth)/password/recovery-password" as Href,
+  SEND_CODE: "/(auth)/password/code" as Href,
+  RESET_PASSWORD: "/(auth)/password/reset-password" as Href,
+  SUCESS: "/(auth)/password/success" as Href,
 } as const;
 
 export type AuthRoutesLink = typeof AuthRoutesLink[keyof typeof AuthRoutesLink];
