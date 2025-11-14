1d6f4790b9db0bc9f8b44aa7fc59ff6bf9b3aa4d
David <dluna.dev@gmail.com>
2025-11-13 21:34:08 -0300

    feat: create spots (mockup)

---
+++ src/constants/spots.ts
@@ -0,0 +1,85 @@
+export const spot = ['Futbol', 'Tenis', 'Basquet', 'Padel', 'Golf', 'Hockey']
+
+export const spot_mocks = [
+  {
+    title: "Cancha 0",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 7500,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 1",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 3200,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 2",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 4999,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 3",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 1567,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 4",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 12000,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 5",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 6000,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 6",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 1200,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 7",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 5000,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+  {
+    title: "Cancha 8",
+    location: "Mar del Plata, Buenos Aires.",
+    stars: 4.8,
+    price: 5000,
+    image_uri: require("@/assets/images/spots.jpg"),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente illo eligendi magnam harum alias sequi molestiae illum, quaerat tempora ipsam eveniet ex rem, beatae animi dolor. Quae vero obcaecati sed?" ,
+    schedules: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "22:00", "23:00"]
+  },
+]
