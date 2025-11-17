f4170c153e763cec02f25bfef09021463e2df5d3
David <dluna.dev@gmail.com>
2025-11-17 06:34:50 -0300

    refactor: add arrow false to Hero component

--- src/app/(auth)/sign-in/index.tsx
+++ src/app/(auth)/sign-in/index.tsx
@@ -1,24 +1,16 @@
-import { View, ImageBackground } from "react-native";
 import React from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
-import { Fab, Text } from "@/components";
-import { ArrowNarrowLeft } from "@/assets/svg";
-import { Colors } from "@/constants/Colors";
-import { useDimensions, useInsets } from "@/hooks";
-import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { Wrapper } from "@/components/";
 import { useTranslation } from "react-i18next";
 import SignInForm from "@/components/organism/form/sign-in.form";
 import { AuthRoutesLink } from "@/utils/enum/routes";
 import { Hero } from "@/components";
+import { useAuth } from "@/context/auth.context";
 
 export default function SignIn() {
-  const { top } = useInsets();
+  const { user } = useAuth();
   const { t } = useTranslation("auth_signin");
-  const { screen_dimensions } = useDimensions();
-
-  const { height } = screen_dimensions;
 
   return (
     <KeyboardAwareScrollView
@@ -28,7 +20,13 @@ export default function SignIn() {
       showsVerticalScrollIndicator={false}
       keyboardShouldPersistTaps="always"
     >
-      <Hero title={t("title")} subtitle={t("sub_title")} route_to={t("sign_up")} redirect_to={() => router.push(AuthRoutesLink.SIGN_UP)} />
+      <Hero
+        title={t("title")}
+        subtitle={t("sub_title")}
+        route_to={t("sign_up")}
+        redirect_to={() => router.push(AuthRoutesLink.SIGN_UP)}
+        arrow={false}
+      />
       <Wrapper className="flex-1">
         <SignInForm />
       </Wrapper>
